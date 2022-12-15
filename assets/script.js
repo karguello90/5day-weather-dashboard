eef2149cacd69c5c9caf953f0e9d46ba3b0b9d50
karguello90 <karguello90@gmail.com>
2022-12-15 15:46:42 -0500

    set up reference function for openweather end point

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 let openWeatherKey = "ad6dc8b5eb7ef3c2731462f979925677";
 
-let presentWeatherPart = 
+let presentWeatherPart = function(selectedCity) {
+    fetch(`api.openweathermap.org `)
+}
 
 let forecastFiveDayPart = 
