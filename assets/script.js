94c1e8103ca9926106d309a6b4f04773a2c537d7
karguello90 <karguello90@gmail.com>
2022-12-16 22:42:29 -0500

    creating fetch for current weather, linked open weatherkey

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 let openWeatherKey = "ad6dc8b5eb7ef3c2731462f979925677";
 
-let presentWeatherPart = function(selectedCity) {
+let presentWeatherPart = function(searchedCity) {
     fetch(`api.openweathermap.org `)
 }
+
+let presentWeatherPart = function(searchedCity)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={ad6dc8b5eb7ef3c2731462f979925677}`)
 
 let forecastFiveDayPart = 
